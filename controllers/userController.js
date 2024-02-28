b51c0e18b2fdd418a93864fbc1eaa4a30695cd8f
itsbhav <118657839+itsbhav@users.noreply.github.com>
2024-02-29 02:10:57 +0530

    cookies update

--- controllers/userController.js
+++ controllers/userController.js
@@ -208,6 +208,7 @@ const createNewUser = async (req, res) => {
       httpOnly: true, //accessible only by web server
       secure: true, //https
       sameSite: "None", //cross-site cookie
+      Partitioned:true,
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
     });
 
